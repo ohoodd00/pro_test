2add8a5042d06c02e02fc9562ae59035ab306cc3
ohoodd00 <ohooodd00@gmail.com>
2024-11-25 18:32:15 +0300

    pushing

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Task#1$$$
+// string myName="Ohoud";
+// int age= 23;
+// double favnum=7;
+// Console.WriteLine($"Hello, my name is {myName}. I am {age} years old, and my favorite number is{favnum}");
+// Task#2
+// Console.WriteLine("Enter 5 numbers:");
+// // int[]inputNum=
+
+// for(int i=0 ;i<6; i++)
+// {
+// int enterNum=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(i);
+// }
+
+// foreach(int i)
+// {
+// Console.WriteLine(i);
+// }
+
+
+//Task#3$$$$
+
+// class Test{
+//  public static void  Main(){
+// Console.WriteLine("Enter number:");
+// int enternum= Convert.ToInt32(Console.ReadLine());
+// IsEven(enternum);
+// void IsEven(int number)
+// {
+
+// if(number/2==0)
+//  Console.WriteLine("The number is even");
+// else
+//  Console.WriteLine("The number is odd");
+
+// }
+
+// }
+// }
+// Task#4
+ Console.WriteLine("enter number  between 1 and 12 ");
+int age= Convert.ToInt32(Console.ReadLine());
+ 
+ 
+    if(age<=12)
+     Console.WriteLine("You are a child");
+     else if(age>=17||age<=13)
+     Console.WriteLine("You are a teenager");
+     else
+     Console.WriteLine("You are an adult");
+    
+// Task#5######
+//  Console.WriteLine("enter number  between 1 and 12 ");
+// int num= Convert.ToInt32(Console.ReadLine());
+// switch(num)
+// {
+//     case 1:
+//      Console.WriteLine("1=January");
+// break;
+//     case 2:
+//      Console.WriteLine("2=February");
+// break;
+//     case 3:
+//      Console.WriteLine("3=March");
+// break;
+//     case 4:
+//      Console.WriteLine("4=Aprill");
+// break;
+//     case 5:
+//      Console.WriteLine("5=May");
+// break;
+//     case 6:
+//      Console.WriteLine("6=June");
+// break;
+//     case 7:
+//      Console.WriteLine("7=July");
+// break;
+//     case 8:
+//      Console.WriteLine("8=Agust");
+// break;
+//     case 9:
+//      Console.WriteLine("9=September");
+// break;
+//     case 10:
+//      Console.WriteLine("10=October");
+// break;
+//     case 11:
+//      Console.WriteLine("11=November");
+// break;
+//     case 12:
+//      Console.WriteLine("12=December");
+// break;
+
+// }
+
+ //Task#6
+
+//  class Test{
+//      public static void  Main(){
+// string[] book=["art","a" ,"b"];
+// //Book.book;
+
+
+//      }
+//      class Book{
+//       string Title;
+//       string Author;
+//       double Price;
+//       public void details(string _Title,string _Auyhor,double _Price){
+//         Title=_Title;
+//         Author=_Auyhor;
+//         Price=_Price;
+//       }
+//       void PrintDetails(){
+//         Console.WriteLine($"Title: {Title}, Author: {Author}, Price: {Price}");
+        
+//       }
+//      }
+
+//  }
+
+
